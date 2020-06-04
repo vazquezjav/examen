0e85d97acaf7bbd004e3557d292390196b3fef20
Bryam David Vega Moreno <vegabryam40@gmail.com>
2020-06-04 11:33:31 -0500

    terminando el trabajo

--- VegaMoreno_Bryam_ExamenMultiprocessing.py
+++ VegaMoreno_Bryam_ExamenMultiprocessing.py
@@ -1,18 +1,78 @@
-import random
-import time
 import multiprocessing
 
-def how_many_max_values_parallel(val):
-    if val == max(ar):
-        return 1
-    else:
-        return 0
+
+import random
+import time
+
+resultPar = 0
+
+def how_many_max_values_sequential(ar):
+
+    #find max value of the list
+
+    maxValue = 0
+    for i in range(len(ar)):
+        if i == 0:
+            maxValue = ar[i]
+        else:
+            if ar[i] > maxValue:
+                maxValue = ar[i]
+
+    #find how many max values are in the list
+    contValue = 0
+    for i in range(len(ar)):
+        if ar[i] == maxValue:
+            contValue += 1
+
+    return contValue
+
+ 
+
+# Complete the how_many_max_values_parallel function below.
+
+def how_many_max_values_parallel(ar):
+    
+    #implement your solution'
+    maxValue = 0
+    for i in range(len(ar)):
+        if i == 0:
+            maxValue = ar[i]
+        else:
+            if ar[i] > maxValue:
+                maxValue = ar[i]
+
+    #find how many max values are in the list
+    contValue = 0
+    for i in range(len(ar)):
+        if ar[i] == maxValue:
+            contValue += 1
+    respuesta.value = contValue
+ 
 
 if __name__ == '__main__':
-    ar_count = 4
+    ar_count = 40000000
+    #Generate ar_count random numbers between 1 and 30
     ar = [random.randrange(1,30) for i in range(ar_count)]
-    inicioPar = time.time()
-    pool                    = multiprocessing.Pool(processes=4)
-    resultPar = sum(list(pool.map(how_many_max_values_parallel,ar)))
-    finPar = time.time()   
+    inicioSec = time.time()
+    resultSec = how_many_max_values_sequential(ar)
+    finSec =  time.time()
+    inicioPar = time.time()   
+    inicio = 0
+    salto = 10000000
+    for i in range(multiprocessing.cpu_count()): 
+        respuesta= multiprocessing.Value("i", 0)
+        process = multiprocessing.Process(target=how_many_max_values_parallel,args = (ar[inicio:salto],))
+        process.start()
+        process.join()
+        inicio = salto
+        salto = salto+10000000
+        resultPar=resultPar+respuesta.value
+    finPar = time.time()
+
+   
+
+    print('Results are correct!\n' if resultSec == resultPar else 'Results are incorrect!\n')
+
+    print('Sequential Process took %.3f ms with %d items\n' % ((finSec - inicioSec)*1000, ar_count))
+
     print('Parallel Process took %.3f ms with %d items\n' % ((finPar - inicioPar)*1000, ar_count))
---
+++ VegaMoreno_Bryam_MPI.py
@@ -0,0 +1,103 @@
+from mpi4py import MPI
+
+import random
+import time
+
+resultPar = 0
+
+def how_many_max_values_sequential(ar):
+
+    #find max value of the list
+
+    maxValue = 0
+    for i in range(len(ar)):
+        if i == 0:
+            maxValue = ar[i]
+        else:
+            if ar[i] > maxValue:
+                maxValue = ar[i]
+
+    #find how many max values are in the list
+    contValue = 0
+    for i in range(len(ar)):
+        if ar[i] == maxValue:
+            contValue += 1
+
+    return contValue
+
+if __name__ == '__main__':
+    FROM_MASTER=1
+    FROM_WORKER=2
+    ar_count = 40000000
+
+    #Generate ar_count random numbers between 1 and 30
+    ar = [random.randrange(1,30) for i in range(ar_count)]
+    
+
+    inicioSec = time.time()
+    resultSec = how_many_max_values_sequential(ar)
+    finSec =  time.time()
+    inicioPar = time.time()  
+    
+    comm = MPI.COMM_WORLD
+    numtasks = comm.size
+    rank = comm.Get_rank()
+    print(rank)
+
+    numworkers = numtasks-1
+    
+    if(rank == 0):
+        averow = len(ar)//numworkers
+        extra = len(ar)%numworkers
+        offset = 0
+        mtype = FROM_MASTER
+        for dest in range(numworkers):
+            if(dest+1 <= extra):
+                rows = averow+1
+            else:
+                rows = averow
+            print("sending %d rows to task %d" %(rows,dest+1));
+            comm.send(offset,dest=dest+1,tag=mtype)
+            comm.send(rows,dest=dest+1,tag=mtype)
+            comm.send(ar[offset:rows+offset],dest=dest+1,tag=mtype)
+            offset = offset + rows
+            
+        resultPar = 0
+        mtype = FROM_WORKER
+        for i in range(numworkers):
+            source = i
+            offset = comm.recv(source=source+1,tag=mtype)
+            rows = comm.recv(source=source+1,tag=mtype)
+            z = comm.recv(source=source+1,tag=mtype)
+            resultPar = resultPar+z
+            
+    if(rank > 0):
+        mtype = FROM_MASTER
+        offset = comm.recv(source=0,tag=mtype)
+        rows = comm.recv(source=0,tag=mtype)
+        ar = comm.recv(source=0,tag=mtype)
+        
+        maxValue = 0
+        for i in range(len(ar)):
+            if i == 0:
+                maxValue = ar[i]
+            else:
+                if ar[i] > maxValue:
+                    maxValue = ar[i]
+
+        #find how many max values are in the list
+        contValue = 0
+        for i in range(len(ar)):
+            if ar[i] == maxValue:
+                contValue += 1
+                
+        mtype = FROM_WORKER
+        comm.send(offset,dest=0,tag=mtype)
+        comm.send(rows,dest=0,tag=mtype)
+        comm.send(contValue,dest=0,tag=mtype)
+    finPar = time.time()
+    print('Results are correct!\n' if resultSec == resultPar else 'Results are incorrect!\n')
+
+    print('Sequential Process took %.3f ms with %d items\n' % ((finSec - inicioSec)*1000, ar_count))
+
+    print('Parallel Process took %.3f ms with %d items\n' % ((finPar - inicioPar)*1000, ar_count)) 
